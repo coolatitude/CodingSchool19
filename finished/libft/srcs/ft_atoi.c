19e86c9f6f6540a0304749639fe324db7a1e5735
pierre <pierre.vanderlynden@gmail.com>
2020-11-30 15:27:35 +0100

    updated atoi

--- finished/libft/srcs/ft_atoi.c
+++ finished/libft/srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pvanderl <pvanderl@students.s19.be>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/04 19:19:15 by pvanderl          #+#    #+#             */
-/*   Updated: 2018/10/20 13:33:53 by pvanderl         ###   ########.fr       */
+/*   Updated: 2020/11/30 15:26:15 by pvanderl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int			ft_atoi(const char *str)
 	int		i;
 	int		sign;
 	long	nbr;
-	int		a;
 
 	sign = 1;
 	nbr = 0;
@@ -29,9 +28,8 @@ int			ft_atoi(const char *str)
 			sign = -1;
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
-		a = nbr;
 		nbr = nbr * 10 + (int)str[i++] - '0';
-		if (a && (a ^ nbr) < 0)
+		if (nbr < 0)
 			return ((sign == 1) ? -1 : 0);
 	}
 	return (sign * nbr);
